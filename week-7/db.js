47fb1ef3ed7aafd94488dd916d78ab08891033ff
kaif tammu <kaiftammu72@gmail.com>
2025-09-23 16:01:20 +0530

    add db.jd

---
+++ week-7/db.js
@@ -0,0 +1,24 @@
+const mongoose = require ("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = ObjectId;
+
+const User = new Schema({
+    name: String,
+    email: {type: String, unique: true},
+    password: String
+});
+
+const Todo = new Schema({
+    userId: ObjectId,
+    title: String,
+    done: Boolean
+});
+
+const userModel = mongoose.model('users',User);
+const todoModel = mongoose.model('todos',Todo);
+
+module.exports = {
+    userModel,
+    todoModel
+}
